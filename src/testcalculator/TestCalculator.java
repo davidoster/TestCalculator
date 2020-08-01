8ddf87bd2bef55db46e4357a6f323ec8c6057aa0
shaGuarGs <67752996+shaGuarGs@users.noreply.github.com>
2020-08-01 13:34:10 +0300

    Update TestCalculator.java

--- src/testcalculator/TestCalculator.java
+++ src/testcalculator/TestCalculator.java
@@ -15,7 +15,7 @@ public class TestCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("");
+        System.out.println("Θέλουμε Παπουτσέλη και ΠΑ.ΣΟ.Κ!");
         System.out.println("");
         System.out.println("");
         System.out.println("");
