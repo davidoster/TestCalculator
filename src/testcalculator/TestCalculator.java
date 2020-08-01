115b05a900ea6b9ec453d0caa6718c8c8752bd65
Kostis <>
2020-08-01 13:42:31 +0300

    Update TestCalculator.java

--- src/testcalculator/TestCalculator.java
+++ src/testcalculator/TestCalculator.java
@@ -15,11 +15,16 @@ public class TestCalculator {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("");
-        System.out.println("");
+        System.out.println("Papoutselis 2");
+=======
+        System.out.println("Θέλουμε Παπουτσέλη και ΠΑ.ΣΟ.Κ!");
+        System.out.println("ΟΙ ΜΟΝΕΣ ΛΥΣΕΙΣ!");
+>>>>>>> 83e85a79b9824ac94e80f5962533d031fbd157b3
         System.out.println("");
         System.out.println("");
         System.out.println("");
     }
-    
+
 }
